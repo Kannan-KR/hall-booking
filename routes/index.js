cfbfbc91c3261ebeedc84b7c13c0028f14610964
Kannan <55331758+Kannan-KR@users.noreply.github.com>
2022-03-10 12:30:18 +0530

    v1

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Hall Booking" });
+});
+
+let roomData = [];
+let bookingData = [];
+
+// create room
+router.post("/create-room", function (req, res, next) {
+  roomData.push(req.body);
+  res.send("Successfully created a new room");
+});
+
+// book room
+router.post("/book-room", function (req, res, next) {
+  let roomIds = [];
+  roomData.forEach((each) => {
+    roomIds.push(each.RoomId);
+  });
+
+  if (roomIds.includes(req.body.RoomId)) {
+    if (bookingData.length === 0) {
+      bookingData.push(req.body);
+      res.send("Successfully booked a room");
+    } else {
+      bookingData.forEach((booking) => {
+        if (
+          booking.RoomId === req.body.RoomId &&
+          booking.Date == req.body.Date
+        ) {
+          res.send("Room is booked for the selected date");
+        } else {
+          bookingData.push(req.body);
+          res.send("Successfully booked a room");
+        }
+      });
+    }
+  } else {
+    res.send("Please enter a valid Room ID");
+  }
+});
+
+// list rooms
+router.get("/list-rooms", function (req, res, next) {
+  res.send(roomData);
+});
+
+// list customers with booking data
+router.get("/list-customers", function (req, res, next) {
+  res.send(bookingData);
+});
+
+module.exports = router;
